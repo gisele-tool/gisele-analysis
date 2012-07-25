355bc8b713351cdf9501f8778f6de797ff3e278b
Bernard Lambeau <blambeau@gmail.com>
2012-07-25 11:35:03 +0200

    Better tests for explicit and simplify guards.

--- spec/glts/test_explicit_guards.rb
+++ spec/glts/test_explicit_guards.rb
@@ -2,14 +2,27 @@ require 'spec_helper'
 module Gisele::Analysis
   describe Glts, 'explicit_guards!' do
 
+    let(:session) do
+      @session = begin
+        s = Session.new
+        s.fluent :moving, ["start"], ["stop"]
+        s.fluent :closed, ["close"], ["open"]
+        s.trackvar :mood, ["measure"]
+        s
+      end
+    end
+
     let(:glts){
-      Glts.new(s) do |g|
+      Glts.new(session) do |g|
         g.add_state :initial => true
         g.add_n_states(4)
-        g.connect(0, 1, :event => :start)
-        g.connect(1, 2, :guard => "closed")
-        g.connect(1, 3, :guard => "not(closed)")
-        g.connect(2, 4, :event => :stop)
+        g.connect(0, 1, :guard => "not(moving)", :event =>"start")
+        g.connect(0, 2, :guard => "moving", :event =>"stop")
+        g.connect(1, 2, :guard => "true", :event =>"stop")
+        g.connect(2, 3, :guard => "mood")
+        g.connect(2, 4, :guard => "not(mood)")
+        g.connect(3, 1, :guard => "true", :event =>"measure")
+        g.connect(4, 1, :guard => "true", :event =>"measure")
       end
     }
 
@@ -23,10 +36,13 @@ module Gisele::Analysis
 
     it 'sets the guards properly' do
       subject
-      guard(0).should eq(bdd "not(moving) and closed")
-      guard(1).should eq(bdd "moving and closed")
-      guard(2).should eq(bdd "false")
-      guard(3).should eq(bdd "moving and closed")
+      guard(0).should eq(bdd "not(moving)")
+      guard(1).should eq(bdd "moving")
+      guard(2).should eq(bdd "true")
+      guard(3).should eq(bdd "not(moving) and mood")
+      guard(4).should eq(bdd "not(moving) and not(mood)")
+      guard(5).should eq(bdd "not(moving) and mood")
+      guard(6).should eq(bdd "not(moving) and not(mood)")
     end
 
   end
--- spec/glts/test_simplify_guards.rb
+++ spec/glts/test_simplify_guards.rb
@@ -2,14 +2,27 @@ require 'spec_helper'
 module Gisele::Analysis
   describe Glts, 'explicit_guards!' do
 
+    let(:session) do
+      @session = begin
+        s = Session.new
+        s.fluent :moving, ["start"], ["stop"]
+        s.fluent :closed, ["close"], ["open"]
+        s.trackvar :mood, ["measure"]
+        s
+      end
+    end
+
     let(:glts){
-      Glts.new(s) do |g|
+      Glts.new(session) do |g|
         g.add_state :initial => true
         g.add_n_states(4)
-        g.connect(0, 1, :event => :start, :guard => "not(moving) and closed")
-        g.connect(1, 2, :guard => "moving and closed")
-        g.connect(1, 3, :guard => "false")
-        g.connect(2, 4, :event => :stop, :guard => "moving and closed")
+        g.connect(0, 1, :guard => "not(moving)", :event =>"start")
+        g.connect(0, 2, :guard => "moving", :event =>"stop")
+        g.connect(1, 2, :guard => "true", :event =>"stop")
+        g.connect(2, 3, :guard => "not(moving) and mood")
+        g.connect(2, 4, :guard => "not(moving) and not(mood)")
+        g.connect(3, 1, :guard => "not(moving) and mood", :event =>"measure")
+        g.connect(4, 1, :guard => "not(moving) and not(mood)", :event =>"measure")
       end
     }
 
@@ -23,10 +36,13 @@ module Gisele::Analysis
 
     it 'sets the guards properly' do
       subject
-      guard(0).should eq(bdd "true")
-      guard(1).should eq(bdd "true")
-      guard(2).should eq(bdd "false")
-      guard(3).should eq(bdd "true")
+      guard(0).should eq(bdd "not(moving)")
+      guard(1).should eq(bdd "moving")
+      guard(2).should eq(bdd "true")
+      guard(3).should eq(bdd "mood")
+      guard(4).should eq(bdd "not(mood)")
+      guard(5).should eq(bdd "true")
+      guard(6).should eq(bdd "true")
     end
 
   end
