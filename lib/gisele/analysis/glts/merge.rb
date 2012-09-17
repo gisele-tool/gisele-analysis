4c0d88d507a01181177fe1c84f2d0297aea035fd
Bernard Lambeau <blambeau@gmail.com>
2012-09-17 13:51:07 +0200

    Revisit merge with aggregators.

--- lib/gisele/analysis/glts/merge.rb
+++ lib/gisele/analysis/glts/merge.rb
@@ -3,6 +3,35 @@ module Gisele::Analysis
 
     class Merge
 
+      STATE_AGGREGATOR = Stamina::Utils::Aggregator.new{|g|
+        g.ignore(:eclosure)
+        g.register(:initial, &:|)
+        g.register(:accepting, &:&)
+        g.register(:error, &:|)
+        g.register(:invariant){|v1,v2|
+          (v1 | v2).ref
+        }
+        g.register(:origin){|v1, v2|
+          v1, v2 = Array(v1), Array(v2)
+          throw :incompatibility unless (v1 & v2).empty?
+          v1 | v2
+        }
+        g.default{|v1,v2|
+          raise "Unexpected state marks: #{v1} vs. #{v2}" unless v1==v2
+          v1
+        }
+      }
+
+      EDGE_AGGREGATOR = Stamina::Utils::Aggregator.new{|g|
+        g.register(:guard){|v1,v2|
+          (v1 | v2).ref
+        }
+        g.default{|v1,v2|
+          raise "Unexpected edge marks: #{v1} vs. #{v2}" unless v1==v2
+          v1
+        }
+      }
+
       def initialize
         @threshold = 2
       end
@@ -43,7 +72,7 @@ module Gisele::Analysis
 
       # Merge `s` and `t` the brute force way (non-recursive, no edge-merge)
       def merge_states(glts, s, t)
-        data = merge_state_data(s.data, t.data)
+        data = STATE_AGGREGATOR.merge(s.data, t.data)
 
         # reconnect `t`'s edges to s
         t.in_edges.each do |in_edge|
@@ -75,40 +104,10 @@ module Gisele::Analysis
           t2 = t2[:representor] while t2[:representor]
 
           # drop edges and reconnect
-          edge_data = merge_edge_data(e.data, f.data)
+          edge_data = EDGE_AGGREGATOR.merge(e.data, f.data)
           glts.drop_edges(e, f)
           new_target = (t1==t2 ? t1 : merge!(glts, t1, t2))
           glts.connect(s, new_target, edge_data)
-        end
-      end
-
-      def merge_state_data(d1, d2)
-        d1.merge(d2) do |k,v1,v2|
-          case k
-          when :invariant
-            (v1 | v2).ref
-          when :origin
-            v1, v2 = Array(v1), Array(v2)
-            throw :incompatibility unless (v1 & v2).empty?
-            v1 | v2
-          when :eclosure
-            nil
-          else
-            raise "Unexpected state marks #{k}: #{v1} vs. #{v2}" unless v1==v2
-            v1
-          end
-        end
-      end
-
-      def merge_edge_data(d1, d2)
-        d1.merge(d2) do |k,v1,v2|
-          case k
-          when :bdd   then (v1 | v2).ref
-          when :guard then "#{v1} | #{v2}"
-          else
-            raise "Unexpected edge marks: #{v1} vs. #{v2}" unless v1==v2
-            v1
-          end
         end
       end
 
